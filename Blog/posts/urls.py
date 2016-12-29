d931e43632583ee11c9df1a369666d968fef2527
BigBorg <770188954@qq.com>
2016-12-29 20:17:48 +0800

    dynamic url routing

--- Blog/posts/urls.py
+++ Blog/posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url("^$", views.post_list),
     url("^create", views.post_create),
-    url("^detail", views.post_detail),
+    url("^detail/(?P<id>\d+)/", views.post_detail),
     url("^list", views.post_list),
     url("^update", views.post_update),
     url("^delete", views.post_delete),
--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -9,8 +9,13 @@ def post_create(request):
     pass
 
 
-def post_detail(request):
-    pass
+def post_detail(request, id):
+    obj = get_object_or_404(Post, id=id)
+    context = {
+        'title': obj.title,
+        'obj': obj
+    }
+    return render(request, "posts/detail.html", context)
 
 
 def post_list(request):
