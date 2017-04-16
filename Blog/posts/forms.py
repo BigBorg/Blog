e6897a1fcd81d739f4082a62df71ff5da2c2da87
BigBorg <770188954@qq.com>
2017-04-16 23:32:34 +0800

    markdown

--- Blog/posts/forms.py
+++ Blog/posts/forms.py
@@ -1,8 +1,10 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Post
+from pagedown.widgets import PagedownWidget
 
-
-class PostForm(ModelForm):
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=PagedownWidget)
+    published = forms.DateField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
         fields = [
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Blog
-My blog using py3, django1.9
-Following tutorial [try-django1.9](https://www.codingforentrepreneurs.com/projects/try-django-19/)
+My blog using py2, django1.9
