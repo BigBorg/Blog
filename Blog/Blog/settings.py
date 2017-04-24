994186a14a3f12dcf687091ac2d923fb42c95794
BigBorg <770188954@qq.com>
2017-04-24 17:22:10 +0800

    mysql setting

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -85,11 +85,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'MYSQL.cnf',
+        },
     }
 }
 
